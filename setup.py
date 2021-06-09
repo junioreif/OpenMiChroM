fbaf20fb40931bdfbcdec0ff9d9f54f10be5f88d
Antonio Bento de Oliveira Junior <junior.phys@gmail.com>
2021-06-09 18:38:56 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 from os import path
 
 this_dir = path.abspath(path.dirname(__file__))
-with open(path.join(this_dir, "README.md")) as f:
+with open(path.join(this_dir, "README.rst")) as f:
     long_description = f.read()
 
 __version__ = "0.0.1"
@@ -22,14 +22,14 @@ setup(
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        'Programming Language :: Python :: 3.4',
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Natural Language :: English",
     ],
     packages=find_packages(),
-    install_requires=['numpy>1.18', 'scipy', 'sklearn', 'six', 'h5py>2', 'openmm>1.6'],
+    install_requires=['numpy>1.18', 'scipy', 'sklearn', 'six', 'h5py>2', 'openmm>7.0'],
     entry_points={"console_scripts": ["CLINAME=OpenMiChroM._cli:main"]},
     zip_safe=True,
     long_description=long_description,
