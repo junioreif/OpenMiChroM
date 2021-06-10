25845fe47e29fcea67bab9932f7fd7f5ea3315fd
Vinícius Contessoto <contessoto@rice.edu>
2021-06-09 23:57:51 -0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,7 +17,7 @@ import sys
 sys.path.insert(0, os.path.abspath(".."))
 sys.path.insert(0, os.path.abspath("../.."))
 sys.path.insert(0, os.path.abspath("../../.."))
-autodoc_mock_imports = ["simtk"]
+autodoc_mock_imports = ["simtk","h5py","numpy","scipy","itertools","pandas","os","sys","time","random","six","sklearn"]
 
 # -- Project information -----------------------------------------------------
 
