9c8a8717539a6bef33aeb502219d00d0174d9b0f
junioreif <junior.phys@gmail.com>
2022-05-26 13:43:29 -0500

    update info to v1.0.3

--- OpenMiChroM/__init__.py
+++ OpenMiChroM/__init__.py
@@ -1,5 +1,5 @@
-__version__ = '1.0.2'
+__version__ = '1.0.3'
 
 from .ChromDynamics import MiChroM
-from .Optimization import FullTraining,CustomMiChroMTraining
+from .Optimization import FullTraining, CustomMiChroMTraining
 from .CndbTools import cndbTools
