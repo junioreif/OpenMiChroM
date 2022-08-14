961fcf994190d66475354bfa05c6ef60f30555b8
s-brahmachari <b.sumitabha@gmail.com>
2022-08-14 12:35:33 -0500

    add cndbtools

---
+++ ActiveMonomer/CndbTools_extended.py
@@ -0,0 +1,8 @@
+import sys
+sys.path.append('/home/sb95/ActiveOpenMiChroM/OpenMiChroM/')
+from CndbTools import cndbTools
+
+class cndbTools_ext:
+    def __init__(self):
+        super(cndbTools_ext, self).__init__()
+         
