27399e8a28b8f9eb44471e1da377378c1453e6fb
junioreif <junior.phys@gmail.com>
2023-06-27 12:15:52 -0500

    fix the classifier tag

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     author="Antonio Bento de Oliveira Junior,Vinicius de Godoi Contessoto",
     author_email="antonio.oliveira@rice.edu,contessoto@rice.edu",
     classifiers=[
-        "License :: MIT",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
         "Topic :: Software Development :: Libraries :: Python Modules",
