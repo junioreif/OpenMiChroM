b3ae3b2dd8531f3ac800b753ee8d4d60cbd327a9
Vinícius Contessoto <contessoto@rice.edu>
2023-06-27 13:52:34 -0500

    Update __init__.py

--- OpenMiChroM/__init__.py
+++ OpenMiChroM/__init__.py
@@ -3,3 +3,4 @@ __version__ = '1.0.7'
 from .ChromDynamics import MiChroM
 from .Optimization import FullTraining, CustomMiChroMTraining
 from .CndbTools import cndbTools
+from .Integrators import ActiveBrownianIntegrator
