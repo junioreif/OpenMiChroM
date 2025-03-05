13446ea2329830d6ff4031e277bd39ceef70ed95
Vinícius Contessoto <contessoto@rice.edu>
2025-03-05 08:39:19 -0600

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,31 +1,25 @@
 # Configuration file for the Sphinx documentation builder.
-#
-# See https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Path setup --------------------------------------------------------------
 
 import os
 import sys
 
-# Assuming conf.py is in docs/source/, this points to project_root/
+# Point to project root for Python modules
 sys.path.insert(0, os.path.abspath("../.."))
 
-# Mock imports for autodoc when dependencies aren't installed
+# Mock imports for autodoc
 autodoc_mock_imports = [
     "simtk", "h5py", "numpy", "scipy", "itertools", "pandas", "os",
     "sys", "time", "random", "six", "sklearn"
 ]
 
-# -- Project information -----------------------------------------------------
-
+# Project information
 project = 'OpenMiChroM'
 copyright = '2020-2025 The Center for Theoretical Biological Physics (CTBP) - Rice University'
 author = 'Antonio B. Oliveira Jr. & Vinícius G. Contessoto'
 version = '1.1.0'
 release = '1.1.0'
 
-# -- General configuration ---------------------------------------------------
-
+# General configuration
 extensions = [
     "sphinx.ext.napoleon",
     "sphinx.ext.autodoc",
@@ -46,25 +40,23 @@ templates_path = ['_templates']
 source_suffix = ".rst"
 master_doc = "index"
 
-exclude_patterns = [
-    "_build",
-    "_templates",
-]
+exclude_patterns = ["_build", "_templates"]
+
+# Dynamically add Tutorials folder to Sphinx source path
+def setup(app):
+    tutorials_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../..', 'Tutorials'))
+    app.srcdir = tutorials_path  # Add Tutorials to source directory
+    app.add_config_value('extra_source_dir', tutorials_path, 'env')
 
 show_authors = True
 pygments_style = "sphinx"
 todo_include_todos = False
 
-# -- Options for HTML output -------------------------------------------------
-
+# HTML output
 html_theme = "sphinx_rtd_theme"
-
-# Static files (e.g., for logo and favicon)
-html_static_path = ['_static']  # Add this to include static files
-html_logo = "_static/images/Open-MichroM_chr1_A549.png"  # Adjusted path
-html_favicon = "_static/images/Open-MichroM_chr1_A549_icon.png"  # Adjusted path
-
+html_static_path = ['_static']
+html_logo = "_static/images/Open-MichroM_chr1_A549.png"
+html_favicon = "_static/images/Open-MichroM_chr1_A549_icon.png"
 html_show_sourcelink = True
 
-# Don’t execute notebooks during build (ensure they’re pre-rendered)
 nbsphinx_execute = 'never'
